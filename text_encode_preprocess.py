6698e462cdd33e773f786b40059cfc44922553bc
Siyang Zhang <siyang_zhang1@brown.edu>
2023-07-04 22:22:54 -0400

    Update text_encode_preprocess.py

--- text_encode_preprocess.py
+++ text_encode_preprocess.py
@@ -2,6 +2,8 @@ import torch
 from transformers import LlamaTokenizer
 from model.modeling_llama import LlamaForCausalLM
 import json
+from tqdm import tqdm
+import traceback
 
 def encode_captions(src_path, dst_path, model_name):
     llama_tokenizer = LlamaTokenizer.from_pretrained(model_name, use_fast=False)
@@ -11,11 +13,10 @@ def encode_captions(src_path, dst_path, model_name):
     with open(src_path, 'r') as json_file:
         data = json.load(json_file)
 
-    for key in data.keys():
+    for key in tqdm(data.keys(), desc='Encoding captions'):
         text = data[key]
         tokens = llama_tokenizer(text, return_tensors="pt", add_special_tokens=False)
         tokens = tokens.input_ids
-
         emb = llama_model.model.embed_tokens(tokens)
         id2emb[key] = emb
 
@@ -23,16 +24,29 @@ def encode_captions(src_path, dst_path, model_name):
 
 if __name__ == '__main__':
 
-    tokenizer = LlamaTokenizer.from_pretrained('lmsys/vicuna-13b-v1.3', use_fast=False)
+    # src_path = '../caption-pdbs/abstract.json'
+    # dst_path = '../caption-pdbs'
 
-    text = 'Fibroblast growth factors (FGFs) are key regulators of cell proliferation, tumor-induced angiogenesis, and migration. FGFs are essential for early embryonic development, organ formation, and angiogenesis. FGF1 also plays an important role in inflammation, wound healing, and restenosis. The biological effects of FGF1 are mediated through the activation of the four transmembrane phosphotyrosine kinase fibroblast growth factor receptors in the presence of heparin sulfate proteoglycans and, therefore, require the release of the protein into the extracellular space. FGF1 is exported through a non-classical release pathway involving the formation of a specific multiprotein complex. The protein constituents of this complex include FGF1, S100A13, and the p40 form of synaptotagmin 1 (Syt1). Because FGF1 plays an important role in tumor formation, it is clear that preventing the formation of the multiprotein complex would be an effective strategy to inhibit a wide range of cancers. To understand the molecular events in the FGF1 release pathway, we studied the FGF1-S100A13 tetrameric and FGF1-S100A13-C2A hexameric complex structures, which are both complexes possibly formed during the non-classical pathway of FGF1 release.'
-    tokens = tokenizer(text, return_tensors="pt", add_special_tokens=False)
-    tokens = tokens.input_ids
-    print('tokens shape:', tokens.shape)
+    # try:
+    #     a = []
+    #     b = a[1]
+    # except Exception as e:
+    #     traceback.print_exc()
 
-    llama_model = LlamaForCausalLM.from_pretrained('lmsys/vicuna-13b-v1.3')
-    emb = llama_model.model.embed_tokens(tokens)
-    print('emb:', emb)
-    print('emb shape:', emb.shape)
+    try:
+        tokenizer = LlamaTokenizer.from_pretrained('lmsys/vicuna-13b-v1.3', use_fast=False)
+
+        text = 'Fibroblast growth factors (FGFs) are key regulators of cell proliferation, tumor-induced angiogenesis, and migration. FGFs are essential for early embryonic development, organ formation, and angiogenesis. FGF1 also plays an important role in inflammation, wound healing, and restenosis. The biological effects of FGF1 are mediated through the activation of the four transmembrane phosphotyrosine kinase fibroblast growth factor receptors in the presence of heparin sulfate proteoglycans and, therefore, require the release of the protein into the extracellular space. FGF1 is exported through a non-classical release pathway involving the formation of a specific multiprotein complex. The protein constituents of this complex include FGF1, S100A13, and the p40 form of synaptotagmin 1 (Syt1). Because FGF1 plays an important role in tumor formation, it is clear that preventing the formation of the multiprotein complex would be an effective strategy to inhibit a wide range of cancers. To understand the molecular events in the FGF1 release pathway, we studied the FGF1-S100A13 tetrameric and FGF1-S100A13-C2A hexameric complex structures, which are both complexes possibly formed during the non-classical pathway of FGF1 release.'
+        tokens = tokenizer(text, return_tensors="pt", add_special_tokens=False)
+        tokens = tokens.input_ids
+        print('tokens shape:', tokens.shape)
+
+        llama_model = LlamaForCausalLM.from_pretrained('lmsys/vicuna-13b-v1.3')
+        emb = llama_model.model.embed_tokens(tokens)
+        print('emb:', emb)
+        print('emb shape:', emb.shape)
+    except Exception as e:
+        print('something wrong:')
+        traceback.print_exc()
 
 
