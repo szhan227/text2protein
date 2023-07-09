0c28e0b41275363df09a47aabc3793b029c003f5
Siyang Zhang <siyang_zhang1@brown.edu>
2023-07-09 14:35:02 -0400

    Update text_preprocess.py

--- text_preprocess.py
+++ text_preprocess.py
@@ -43,8 +43,13 @@ if __name__ == '__main__':
         print('start to test model:', model_name)
         tokenizer = LlamaTokenizer.from_pretrained(model_name, use_fast=False)
         llama_model = LlamaForCausalLM.from_pretrained(model_name)
-        llama_model = llama_model.to(device)
-        print('Successfully loaded llm model:', model_name)
+        print('Successfully loaded llm model on cpu:', model_name)
+
+        try:
+            llama_model = llama_model.to(device)
+        except:
+            device = 'cpu'
+            print('Failed to load llm model on cuda, use cpu instead')
 
         test_text = 'Fibroblast growth factors (FGFs) are key regulators of cell proliferation, tumor-induced angiogenesis, and migration. FGFs are essential for early embryonic development, organ formation, and angiogenesis. FGF1 also plays an important role in inflammation, wound healing, and restenosis. The biological effects of FGF1 are mediated through the activation of the four transmembrane phosphotyrosine kinase fibroblast growth factor receptors in the presence of heparin sulfate proteoglycans and, therefore, require the release of the protein into the extracellular space. FGF1 is exported through a non-classical release pathway involving the formation of a specific multiprotein complex. The protein constituents of this complex include FGF1, S100A13, and the p40 form of synaptotagmin 1 (Syt1). Because FGF1 plays an important role in tumor formation, it is clear that preventing the formation of the multiprotein complex would be an effective strategy to inhibit a wide range of cancers. To understand the molecular events in the FGF1 release pathway, we studied the FGF1-S100A13 tetrameric and FGF1-S100A13-C2A hexameric complex structures, which are both complexes possibly formed during the non-classical pathway of FGF1 release.'
         test_toks = tokenizer(test_text, return_tensors="pt", add_special_tokens=False, max_length=512, padding='max_length').to(device)
