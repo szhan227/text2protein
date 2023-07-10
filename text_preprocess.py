e612606ce8a46b413b06bf015d16dcf27d603222
Siyang Zhang <siyang_zhang1@brown.edu>
2023-07-10 18:04:34 -0400

    Update text_preprocess.py

--- text_preprocess.py
+++ text_preprocess.py
@@ -36,6 +36,7 @@ if __name__ == '__main__':
 
     try:
         # device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        print('number of GPU:', torch.cuda.device_count())
         device = 'cpu'
         print('test preprocess on device:', device)
 
